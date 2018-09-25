e3734fe3db9fe783ca84a1ed324dcbd36a54b244
Judith <j.r.lloydjones@gmail.com>
2018-09-25 09:53:08 +0000

    created methods print_menu, process, show_students and changed to @students

--- directory.rb
+++ directory.rb
@@ -1,37 +1,49 @@
 # array of students
 
 def interactive_menu
-  students = []
+  @students = [] # an empty array accessible to all methods
   loop do
-    # prints the menu and asks the user what they want to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # read the input and saves it to a variable
-    selection = gets.chomp
-    # do what the user has asked
-    case selection
+    print_menu
+    process(gets.chomp) # takes user input as argument
+  end 
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
     when "1"
-        students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you mean, please try again"
-    end 
   end 
 end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
 def input_students
   # create an empty array
-  students = []
   puts "Do you want to add a student to the directory? (Y/N)"
   answer = gets.chomp.upcase
   until answer != "Y"
     puts "Please enter the name of the student and then press enter."
-    name = gets.chomp
+    name = gets.chomp.capitalize
+    # if nothing entered name is set to unknown
+      if name == ""
+        name = "unknown"
+      end
     puts "Please enter their cohort"
     cohort = gets.chomp.downcase
     # if nothing entered default cohort is set as May
@@ -45,45 +57,36 @@ def input_students
         age = "unknown"
       end
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, age: age}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym, age: age}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     # get another name from the user
     puts "Do you want to add another student? (Y/N)"
     answer = gets.chomp.upcase
   end
-    # return the array of students
-    students 
 end
+
 def print_header
     puts "The students of Villains Academy"
     puts "--------------------------------"
 end
-def print(students)
-  students.each_with_index do |(student), index|    
+
+def print_student_list
+  @students.each_with_index do |(student), index|    
     puts "#{index+1} #{student[:name].center(20)} (#{student[:cohort]} cohort)"
   end
 end 
-#def print_by_cohort(students)
-#  students_by_cohort = Hash.new 
-#  students.map do |student|
-#    if students_by_cohort[student[:cohort]] == nil
-#      students_by_cohort[student[:cohort]] = [student[:name]]
-#    else 
-#      students_by_cohort[student[:cohort]].push(student[:name])
-#    end
-#  end
-#  students_by_cohort.map do |key, value|
-#    puts "#{key.upcase}"
-#    value.map{|name| puts name}
-#  end
-#end 
-def print_footer(students)
-  if students.count == 0
+
+def print_footer
+  if @students.count == 0
     puts "There are no students enrolled at Villains Academy"
-  elsif students.count == 1
-    puts "Overall we have #{students.count} great student"
+  elsif @students.count == 1
+    puts "Overall we have #{@students.count} great student"
   else
-    puts "Overall we have #{students.count} great students"
+    puts "Overall we have #{@students.count} great students"
   end
 end
 
